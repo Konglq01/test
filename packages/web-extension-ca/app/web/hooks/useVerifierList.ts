b72557da8a5899ebe97fadb8e14c1c4f67735147
potter-sun <potter.sun@aelf.io>
2022-12-27 15:53:07 +0800

    feat: 🎸 add getVerifierList

--- packages/web-extension-ca/app/web/hooks/useVerifierList.ts
+++ packages/web-extension-ca/app/web/hooks/useVerifierList.ts
@@ -1,11 +1,28 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from 'store/Provider/hooks';
-import { fetchVerifierListAsync } from '@portkey/store/store-ca/guardians/actions';
+import { setVerifierListAction } from '@portkey/store/store-ca/guardians/actions';
+import { getVerifierList } from 'utils/sandboxUtil/getVerifierList';
 
 const useVerifierList = () => {
   const dispatch = useAppDispatch();
   useEffect(() => {
-    dispatch(fetchVerifierListAsync());
+    getVerifierList({
+      rpcUrl: 'http://192.168.67.77:8000',
+      address: '2LUmicHyH4RXrMjG4beDwuDsiWJESyLkgkwPdGTR8kahRzq5XS',
+      chainType: 'aelf',
+    })
+      .then((res) => {
+        console.log(res, 'getVerifierList===');
+        const verifierList = res.result.verifierList?.map((item: any) => ({
+          name: item.name,
+          url: item.endPoints[0],
+          imageUrl: item.imageUrl,
+        }));
+        dispatch(setVerifierListAction(verifierList));
+      })
+      .catch((err) => {
+        console.error(err, 'useVerifierList===error');
+      });
   }, [dispatch]);
 };
 
