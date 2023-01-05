ac4daae371bd3f500632e32d4520c5eb0635ebc1
portkey-yellow <yellow.m@portkey.finance>
2023-01-05 14:20:53 +0800

    fix: 🐛 hooks

--- packages/hooks/hooks-ca/graphql.ts
+++ packages/hooks/hooks-ca/graphql.ts
@@ -2,27 +2,40 @@ import useInterval from '../useInterval';
 import { DefaultChainId } from '@portkey/constants/constants-ca/network';
 import { contractQueries } from '@portkey/graphql/index';
 import { NetworkType } from '@portkey/types';
-import { useState } from 'react';
-import { CAInfo } from '@portkey/types/types-ca/wallet';
-export function useIntervalQueryManagerInfo(network: NetworkType, address?: string) {
-  const [caInfo, setCAInfo] = useState<{ [address: string]: CAInfo }>();
+import { useState, useMemo } from 'react';
+import { CAInfoType, LoginType, ManagerInfo } from '@portkey/types/types-ca/wallet';
+import { VerificationType } from '@portkey/types/verifier';
+export function useIntervalQueryCAInfoByAddress(network: NetworkType, address?: string) {
+  const [info, setInfo] = useState<{ [address: string]: CAInfoType }>();
+  const caInfo = useMemo(() => (address && info ? info?.[address + network] : undefined), [info, address]);
   useInterval(
     async () => {
-      if (!address) return;
+      if (!address || caInfo) return;
       try {
         const { caHolderManagerInfo } = await contractQueries.getCAHolderByManager(network, {
           chainId: DefaultChainId,
           manager: address,
         });
-        console.log(caHolderManagerInfo, '=====caHolderManagerInfo');
-        const { caAddress, caHash } = caHolderManagerInfo[0];
-        if (caAddress && caHash) setCAInfo({ address: { caAddress, caHash } });
+        const { caAddress, caHash, loginGuardianTypeInfo } = caHolderManagerInfo[0];
+        if (caAddress && caHash)
+          setInfo({
+            [address + network]: {
+              managerInfo: {
+                managerUniqueId: loginGuardianTypeInfo[0].id,
+                loginGuardianType: loginGuardianTypeInfo[0].loginGuardianType,
+                //TODO: dynamic type
+                type: LoginType.email,
+                verificationType: VerificationType.addManager,
+              } as ManagerInfo,
+              [DefaultChainId]: { caAddress, caHash },
+            },
+          });
       } catch (error) {
         console.log(error, '=====error');
       }
     },
-    2000,
-    [],
+    3000,
+    [caInfo, network, address],
   );
-  return address && caInfo ? caInfo[address] : undefined;
+  return caInfo;
 }
