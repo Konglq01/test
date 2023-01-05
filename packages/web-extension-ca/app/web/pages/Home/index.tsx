4d7e3fa70f4a45d8239485168ddf0e3606beb310
Ian-potter <Ian@portkey.finance>
2023-01-05 18:51:26 +0800

    refactor: 💡 scan

--- packages/web-extension-ca/app/web/pages/Home/index.tsx
+++ packages/web-extension-ca/app/web/pages/Home/index.tsx
@@ -1,21 +1,73 @@
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 import PortKeyHeader from 'pages/components/PortKeyHeader';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback } from 'react';
 import { useNavigate } from 'react-router';
 import MyBalance from './components/MyBalance';
 import './index.less';
-import { useCommonState } from 'store/Provider/hooks';
-import InternalMessage from 'messages/InternalMessage';
-import InternalMessageTypes from 'messages/InternalMessageTypes';
 import './index.less';
+import { addGuardian } from 'utils/sandboxUtil/addGuardian';
+import { useCurrentNetworkInfo } from '@portkey/hooks/hooks-ca/network';
+import { useCurrentChain } from '@portkey/hooks/hooks-ca/chainList';
+import getPrivateKeyAndMnemonic from 'utils/Wallet/getPrivateKeyAndMnemonic';
+import { useCurrentWallet } from '@portkey/hooks/hooks-ca/wallet';
 
 export default function Home() {
   const navigate = useNavigate();
+  const currentNetwork = useCurrentNetworkInfo();
+  const currentChain = useCurrentChain();
+  const wallet = useCurrentWallet();
+  // 2A7tQJt8LgTPDvExTRSKdXQxgKWBcudSaUgBSkh7BPwxbHHMNw
+  console.log(wallet, 'walletInfo==');
   const TestCode = (
     <Button
       onClick={async () => {
         // await InternalMessage.payload(InternalMessageTypes.SET_SEED, '11111111').send();
-        const seed = await InternalMessage.payload(InternalMessageTypes.GET_SEED).send();
+        const res = await getPrivateKeyAndMnemonic(
+          {
+            AESEncryptPrivateKey: wallet.walletInfo.AESEncryptPrivateKey,
+          },
+          '11111111',
+        );
+        if (!currentChain?.endPoint || !res?.privateKey) return message.error('error');
+        const seed = await addGuardian({
+          rpcUrl: currentChain.endPoint,
+          chainType: currentNetwork.walletType,
+          address: currentChain.caContractAddress,
+          privateKey: '8aa27d601e2dfd823b5b94e83ff1ae43e7cc1f6fe51702ce33e1c98d39540d8f',
+          paramsOption: [
+            {
+              caHash: '2045f9b4859d0b9eb6015ec90cabdfb31939ae19500ef0b9970aade32f310650',
+              guardianToAdd: {
+                guardianType: {
+                  type: 0,
+                  guardianType: 'hong.lin@aelf.io',
+                },
+                verifier: {
+                  name: 'Verifier-002',
+                  signature:
+                    '4be2650fcc49f2e47c510a19b076a409c72e79ab128afcef66bdc934550cc1fe7a78d14fe9104d82f50ccd7fa2c7fe31517e8bb181b88dee48364e5f25414ba401',
+                  verificationDoc:
+                    '0,hong.lin@aelf.io,01/05/2023 10:09:27,2mBnRTqXMb5Afz4CWM2QakLRVDfaq2doJNRNQT1MXoi2uc6Zy3',
+                },
+              },
+              guardiansApproved: [
+                {
+                  guardianType: {
+                    type: 0,
+                    guardianType: 'hong.lin@hoopox.com',
+                  },
+                  verifier: {
+                    name: 'Verifier-002',
+                    signature:
+                      '38055b3a15df378571a5a19787dced9123ebbedfdb60b80dd533361e536b870000679977bbd8ecf691017678013f7622e5ac5a5fa303038a56692be6a8de06e301',
+                    verificationDoc:
+                      '0,hong.lin@hoopox.com,01/05/2023 10:09:45,2mBnRTqXMb5Afz4CWM2QakLRVDfaq2doJNRNQT1MXoi2uc6Zy3',
+                  },
+                },
+              ],
+            },
+          ],
+        });
         console.log(seed, 'Check==');
       }}>
       Check
@@ -31,7 +83,7 @@ export default function Home() {
       <PortKeyHeader onUserClick={onUserClick} />
       <div className="portkey-home-body">
         <MyBalance />
-        {/* {TestCode} */}
+        {TestCode}
         {/* <SettingDrawer
           open={visible}
           onClose={() => setVisible(false)}
--- packages/web-extension-ca/app/web/pages/RegisterStart/index.tsx
+++ packages/web-extension-ca/app/web/pages/RegisterStart/index.tsx
@@ -13,6 +13,11 @@ import { NetworkType } from '@portkey/types';
 import CommonSelect from 'components/CommonSelect1';
 import { useChangeNetwork } from 'hooks/useChangeNetwork';
 import i18n from 'i18n';
+import { Button, message } from 'antd';
+import getPrivateKeyAndMnemonic from 'utils/Wallet/getPrivateKeyAndMnemonic';
+import { addGuardian } from 'utils/sandboxUtil/addGuardian';
+import { useCurrentChain } from '@portkey/hooks/hooks-ca/chainList';
+import { useCurrentWallet } from '@portkey/hooks/hooks-ca/wallet';
 
 export default function RegisterStart() {
   const { type } = useParams();
@@ -40,6 +45,64 @@ export default function RegisterStart() {
       network && changeNetwork(network);
     },
     [changeNetwork, dispatch, networkList],
+  );
+
+  const currentChain = useCurrentChain();
+  const wallet = useCurrentWallet();
+  const TestCode = (
+    <Button
+      onClick={async () => {
+        // await InternalMessage.payload(InternalMessageTypes.SET_SEED, '11111111').send();
+        // const res = await getPrivateKeyAndMnemonic(
+        //   {
+        //     AESEncryptPrivateKey: wallet.walletInfo.AESEncryptPrivateKey,
+        //   },
+        //   '11111111',
+        // );
+        if (!currentChain?.endPoint) return message.error('error');
+        const seed = await addGuardian({
+          rpcUrl: currentChain.endPoint,
+          chainType: currentNetwork.walletType,
+          address: currentChain.caContractAddress,
+          privateKey: '8aa27d601e2dfd823b5b94e83ff1ae43e7cc1f6fe51702ce33e1c98d39540d8f',
+          paramsOption: [
+            {
+              caHash: '2045f9b4859d0b9eb6015ec90cabdfb31939ae19500ef0b9970aade32f310650',
+              guardianToAdd: {
+                guardianType: {
+                  type: 0,
+                  guardianType: 'hong.lin@aelf.io',
+                },
+                verifier: {
+                  name: 'Verifier-002',
+                  signature:
+                    '4be2650fcc49f2e47c510a19b076a409c72e79ab128afcef66bdc934550cc1fe7a78d14fe9104d82f50ccd7fa2c7fe31517e8bb181b88dee48364e5f25414ba401',
+                  verificationDoc:
+                    '0,hong.lin@aelf.io,01/05/2023 10:09:27,2mBnRTqXMb5Afz4CWM2QakLRVDfaq2doJNRNQT1MXoi2uc6Zy3',
+                },
+              },
+              guardiansApproved: [
+                {
+                  guardianType: {
+                    type: 0,
+                    guardianType: 'hong.lin@hoopox.com',
+                  },
+                  verifier: {
+                    name: 'Verifier-002',
+                    signature:
+                      '38055b3a15df378571a5a19787dced9123ebbedfdb60b80dd533361e536b870000679977bbd8ecf691017678013f7622e5ac5a5fa303038a56692be6a8de06e301',
+                    verificationDoc:
+                      '0,hong.lin@hoopox.com,01/05/2023 10:09:45,2mBnRTqXMb5Afz4CWM2QakLRVDfaq2doJNRNQT1MXoi2uc6Zy3',
+                  },
+                },
+              ],
+            },
+          ],
+        });
+        console.log(seed, 'Check==');
+      }}>
+      Check
+    </Button>
   );
 
   return (
@@ -71,6 +134,7 @@ export default function RegisterStart() {
           )}
         </div>
       </div>
+      {TestCode}
     </div>
   );
 }
