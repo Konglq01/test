9be13c07d10356f753fc7c94ee6330300b69f475
portkey-y <yangkexin@portkey.finance>
2023-01-17 19:42:20 +0800

    test: update UT config

--- jest.config.js
+++ jest.config.js
@@ -2,26 +2,26 @@
 module.exports = {
   displayName: 'portkey',
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   coverageDirectory: 'coverage',
-  // testMatch: [
-  //   '<rootDir>/packages/hooks/**/*.test.ts',
-  //   '<rootDir>/packages/store/**/*.test.ts'
-  // ],
-  projects: [
-    {
-      displayName: 'hooks',
-      preset: 'ts-jest',
-      testMatch: [
-        '<rootDir>/packages/hooks/**/*.test.ts'
-      ]
-    },
-    {
-      displayName: 'store',
-      preset: 'ts-jest',
-      testMatch: [
-        '<rootDir>/packages/store/**/*.test.ts'
-      ]
-    }
-  ]
+  testMatch: ['<rootDir>/test/**/*.test.tsx'],
+  // projects: [
+  //   {
+  //     displayName: 'hooks',
+  //     preset: 'ts-jest',
+  //     testMatch: [
+  //       '<rootDir>/packages/hooks/**/*.test.tsx'
+  //     ],
+  //     testEnvironment: 'jsdom',
+  //   },
+  //   {
+  //     displayName: 'store',
+  //     preset: 'ts-jest',
+  //     testMatch: [
+  //       '<rootDir>/packages/store/**/*.test.tsx'
+  //     ],
+  //     testEnvironment: 'jsdom',
+  //   }
+  // ]
+
 };
