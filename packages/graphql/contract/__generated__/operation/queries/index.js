2e0d397cf03361f9d538428bb2571159a245ceb1
thomas-portkey <thomas@portkey.finance>
2023-01-30 17:33:35 +0800

    feat: 🎸 format graphql automatic file

--- packages/graphql/contract/__generated__/operation/queries/index.js
+++ packages/graphql/contract/__generated__/operation/queries/index.js
@@ -9,6 +9,15 @@ module.exports.loginGuardianTypeInfo = fs.readFileSync(path.join(__dirname, 'log
 module.exports.userNFTProtocolInfo = fs.readFileSync(path.join(__dirname, 'userNFTProtocolInfo.gql'), 'utf8');
 module.exports.userNFTInfo = fs.readFileSync(path.join(__dirname, 'userNFTInfo.gql'), 'utf8');
 module.exports.caHolderTokenBalanceInfo = fs.readFileSync(path.join(__dirname, 'caHolderTokenBalanceInfo.gql'), 'utf8');
-module.exports.caHolderTransactionAddressInfo = fs.readFileSync(path.join(__dirname, 'caHolderTransactionAddressInfo.gql'), 'utf8');
-module.exports.loginGuardianTypeChangeRecordInfo = fs.readFileSync(path.join(__dirname, 'loginGuardianTypeChangeRecordInfo.gql'), 'utf8');
-module.exports.caHolderManagerChangeRecordInfo = fs.readFileSync(path.join(__dirname, 'caHolderManagerChangeRecordInfo.gql'), 'utf8');
+module.exports.caHolderTransactionAddressInfo = fs.readFileSync(
+  path.join(__dirname, 'caHolderTransactionAddressInfo.gql'),
+  'utf8',
+);
+module.exports.loginGuardianTypeChangeRecordInfo = fs.readFileSync(
+  path.join(__dirname, 'loginGuardianTypeChangeRecordInfo.gql'),
+  'utf8',
+);
+module.exports.caHolderManagerChangeRecordInfo = fs.readFileSync(
+  path.join(__dirname, 'caHolderManagerChangeRecordInfo.gql'),
+  'utf8',
+);
--- packages/graphql/contract/hooks/loginGuardianTypeChangeRecordInfo.ts
+++ packages/graphql/contract/hooks/loginGuardianTypeChangeRecordInfo.ts
@@ -7,22 +7,31 @@ export type LoginGuardianTypeChangeRecordInfoQueryVariables = Types.Exact<{
   dto?: Types.InputMaybe<Types.GetLoginGuardianTypeChangeRecordDto>;
 }>;
 
-
-export type LoginGuardianTypeChangeRecordInfoQuery = { __typename?: 'Query', loginGuardianTypeChangeRecordInfo?: Array<{ __typename?: 'LoginGuardianTypeChangeRecordDto', id?: string | null, caHash?: string | null, caAddress?: string | null, changeType?: string | null, loginGuardianType?: string | null, blockHeight: number } | null> | null };
-
+export type LoginGuardianTypeChangeRecordInfoQuery = {
+  __typename?: 'Query';
+  loginGuardianTypeChangeRecordInfo?: Array<{
+    __typename?: 'LoginGuardianTypeChangeRecordDto';
+    id?: string | null;
+    caHash?: string | null;
+    caAddress?: string | null;
+    changeType?: string | null;
+    loginGuardianType?: string | null;
+    blockHeight: number;
+  } | null> | null;
+};
 
 export const LoginGuardianTypeChangeRecordInfoDocument = gql`
-    query loginGuardianTypeChangeRecordInfo($dto: GetLoginGuardianTypeChangeRecordDto) {
-  loginGuardianTypeChangeRecordInfo(dto: $dto) {
-    id
-    caHash
-    caAddress
-    changeType
-    loginGuardianType
-    blockHeight
+  query loginGuardianTypeChangeRecordInfo($dto: GetLoginGuardianTypeChangeRecordDto) {
+    loginGuardianTypeChangeRecordInfo(dto: $dto) {
+      id
+      caHash
+      caAddress
+      changeType
+      loginGuardianType
+      blockHeight
+    }
   }
-}
-    `;
+`;
 
 /**
  * __useLoginGuardianTypeChangeRecordInfoQuery__
@@ -40,14 +49,37 @@ export const LoginGuardianTypeChangeRecordInfoDocument = gql`
  *   },
  * });
  */
-export function useLoginGuardianTypeChangeRecordInfoQuery(baseOptions?: Apollo.QueryHookOptions<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>(LoginGuardianTypeChangeRecordInfoDocument, options);
-      }
-export function useLoginGuardianTypeChangeRecordInfoLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>(LoginGuardianTypeChangeRecordInfoDocument, options);
-        }
-export type LoginGuardianTypeChangeRecordInfoQueryHookResult = ReturnType<typeof useLoginGuardianTypeChangeRecordInfoQuery>;
-export type LoginGuardianTypeChangeRecordInfoLazyQueryHookResult = ReturnType<typeof useLoginGuardianTypeChangeRecordInfoLazyQuery>;
-export type LoginGuardianTypeChangeRecordInfoQueryResult = Apollo.QueryResult<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>;
+export function useLoginGuardianTypeChangeRecordInfoQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    LoginGuardianTypeChangeRecordInfoQuery,
+    LoginGuardianTypeChangeRecordInfoQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>(
+    LoginGuardianTypeChangeRecordInfoDocument,
+    options,
+  );
+}
+export function useLoginGuardianTypeChangeRecordInfoLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    LoginGuardianTypeChangeRecordInfoQuery,
+    LoginGuardianTypeChangeRecordInfoQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<LoginGuardianTypeChangeRecordInfoQuery, LoginGuardianTypeChangeRecordInfoQueryVariables>(
+    LoginGuardianTypeChangeRecordInfoDocument,
+    options,
+  );
+}
+export type LoginGuardianTypeChangeRecordInfoQueryHookResult = ReturnType<
+  typeof useLoginGuardianTypeChangeRecordInfoQuery
+>;
+export type LoginGuardianTypeChangeRecordInfoLazyQueryHookResult = ReturnType<
+  typeof useLoginGuardianTypeChangeRecordInfoLazyQuery
+>;
+export type LoginGuardianTypeChangeRecordInfoQueryResult = Apollo.QueryResult<
+  LoginGuardianTypeChangeRecordInfoQuery,
+  LoginGuardianTypeChangeRecordInfoQueryVariables
+>;
