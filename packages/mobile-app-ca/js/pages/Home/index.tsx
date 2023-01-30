1fe9e66387a2d5179e0348dc4eec30c65e648caa
portkey-yellow <yellow.m@portkey.finance>
2023-01-30 14:15:34 +0800

    feat: 🎸 deepCompareMemo

--- packages/mobile-app-ca/js/pages/Home/index.tsx
+++ packages/mobile-app-ca/js/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Button, Text } from '@rneui/base';
 import { ScrollView } from 'react-native-gesture-handler';
 import navigationService from '../../utils/navigationService';
