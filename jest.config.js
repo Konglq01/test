a570b671c38350850781d86b1c78f5d2212cb03d
ykx <yangkexin@portkey.finance>
2023-02-06 11:22:54 +0800

    test: init jest env

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  displayName: 'portkey',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  coverageDirectory: 'coverage',
+  testMatch: [
+    '<rootDir>/packages/hooks/**/*.test.{ts,tsx}',
+    '<rootDir>/packages/utils/**/*.test.{ts,tsx}',
+    '<rootDir>/packages/store/**/*.test.{ts,tsx}',
+    '<rootDir>/packages/web-extension-ca/**/*.test.tsx',
+  ],
+  // testMatch: ['<rootDir>/test/**/*.test.tsx', '<rootDir>/packages/hooks/**/*.test.tsx'],
+  collectCoverageFrom: [
+    '**/packages/hooks/hooks-ca/**/*.{ts,tsx}',
+    'packages/web-extension-ca/app/web/components/DropdownSearch/*.{ts,tsx}',
+    '**/packages/store/store-ca/guardians/*.{ts,tsx}',
+    '**/packages/utils/wallet/*.ts',
+    '!**/node_modules/**',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  // projects: [
+  //   {
+  //     displayName: 'hooks',
+  //     preset: 'ts-jest',
+  //     testMatch: [
+  //       '<rootDir>/packages/hooks/**/*.test.tsx'
+  //     ],
+  //     testEnvironment: 'jsdom',
+  //   },
+  //   {
+  //     displayName: 'store',
+  //     preset: 'ts-jest',
+  //     testMatch: [
+  //       '<rootDir>/packages/store/**/*.test.tsx'
+  //     ],
+  //     testEnvironment: 'jsdom',
+  //   }
+  // ]
+};
