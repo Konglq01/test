466002c41dda371ee7d9f0c268426423e25f513b
portkey-yellow <yellow.m@portkey.finance>
2023-02-28 19:31:08 +0800

    fix: 🐛 biometrics

--- packages/hooks/mobile.ts
+++ packages/hooks/mobile.ts
@@ -1,6 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { authenticationReady } from '@portkey/utils/mobile/authentication';
+import { useEffect, useRef, useCallback } from 'react';
 import { BackHandler } from 'react-native';
-
+import { setBiometrics } from 'store/user/actions';
+import { useAppCommonDispatch } from './index';
 export function useHardwareBackPress(callback?: () => boolean) {
   const savedCallback = useRef<() => boolean>();
   useEffect(() => {
@@ -25,4 +27,15 @@ export function usePreventHardwareBack(callback?: () => void) {
     savedCallback.current?.();
     return true;
   });
+}
+
+export function useSetBiometrics() {
+  const dispatch = useAppCommonDispatch();
+  return useCallback(async (value: boolean) => {
+    if (value) {
+      const isReady = await authenticationReady();
+      if (!isReady) throw new Error('biometrics is not ready');
+    }
+    dispatch(setBiometrics(value));
+  }, []);
 }
