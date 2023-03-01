3c87ecdc451dfcea75e1700c46b5663aefb16558
Portkey-David <alex@portkey.finance>
2023-03-01 11:44:09 +0800

    fix: 🐛 activity detail

--- packages/mobile-app-ca/js/pages/Activity/ActivityDetail/index.tsx
+++ packages/mobile-app-ca/js/pages/Activity/ActivityDetail/index.tsx
@@ -67,6 +67,8 @@ const ActivityDetail = () => {
 
   const isNft = useMemo(() => !!activityItem?.nftInfo?.nftId, [activityItem?.nftInfo?.nftId]);
   const status = useMemo(() => {
+    if (!activityItem?.status) return { text: '', style: 'confirmed' };
+
     if (activityItem?.status === TransactionStatus.Mined)
       return {
         text: 'Confirmed',
@@ -240,6 +242,7 @@ const ActivityDetail = () => {
           containerStyle={[GStyles.marginTop(pTd(8)), styles.bottomButton]}
           onPress={() => {
             if (!explorerUrl) return;
+            if (!activityItem?.transactionId) return;
 
             navigationService.navigate('ViewOnWebView', {
               url: getExploreLink(explorerUrl, activityItem?.transactionId || '', 'transaction'),
