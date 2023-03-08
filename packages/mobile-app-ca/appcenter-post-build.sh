0dd7e5f0cbe0565eb2a24beb2ed5d9bee1a94aff
Portkey-David <alex@portkey.finance>
2023-03-08 14:46:41 +0800

    fix: 🐛 change the notification to English

--- packages/mobile-app-ca/appcenter-post-build.sh
+++ packages/mobile-app-ca/appcenter-post-build.sh
@@ -3,29 +3,26 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     # Example: Upload main branch app binary to HockeyApp using the API
     if [ "$PLATFORM_TYPE" == "android" ];
      then
-        # curl -X POST -H "Content-Type: application/json" \
-        # -d '{"msg_type":"text","content":{"text":"DID:ANDROID端最新包 （id:'$APPCENTER_BUILD_ID'） build成功, 可在appcenter中下载最新包进行测试  https://install.appcenter.ms/orgs/aelf-web/apps/DID-Android"}}' \
-        # https://open.feishu.cn/open-apis/bot/v2/hook/f2d3fffd-c630-4e59-86e3-e7053a64e4b2
         curl -X POST -H "Content-Type: application/json" \
         -d '{
             "msg_type": "post",
             "content": {
                 "post": {
                     "zh_cn": {
-                        "title": "代码包更新通知",
+                        "title": "Notification about package updatting",
                         "content": [
                             [{
                                     "tag": "text",
-                                    "text": "最新Android代码包（id:'$APPCENTER_BUILD_ID'）已经更新: "
+                                    "text": "The latest android package（id:'$APPCENTER_BUILD_ID'）has updated: "
                                 },
                                 {
                                     "tag": "a",
-                                    "text": "点击此处",
+                                    "text": "click here",
                                     "href": "https://install.appcenter.ms/orgs/aelf-web/apps/DID-Android"
                                 },
                                       {
                                     "tag": "text",
-                                    "text": "进行查看和下载"
+                                    "text": "check and download"
                                 },
                                 {   
                                     "tag": "img",
@@ -46,30 +43,27 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     # Example: Upload main branch app binary to HockeyApp using the API
     if [ "$PLATFORM_TYPE" == "ios" ];
      then
-        # curl -X POST -H "Content-Type: application/json" \
-        # -d '{"msg_type":"text","content":{"text":"DID:IOS端最新包（id:'$APPCENTER_BUILD_ID'） build成功, 可在appcenter中下载最新包进行测试 https://install.appcenter.ms/orgs/aelf-web/apps/DID-IOS"}}' \
-        # https://open.feishu.cn/open-apis/bot/v2/hook/f2d3fffd-c630-4e59-86e3-e7053a64e4b2
         curl -X POST -H "Content-Type: application/json" \
         -d '{
             "msg_type": "post",
             "content": {
                 "post": {
                     "zh_cn": {
-                        "title": "代码包更新通知",
+                        "title": "Notification about package updatting",
                         "content": [
                             [
                                 {
                                     "tag": "text",
-                                    "text": "最新IOS代码包（id:'$APPCENTER_BUILD_ID'）已经更新，"
+                                    "text": "The latest ios package(id:'$APPCENTER_BUILD_ID')has updated: "
                                 },
                                 {
                                     "tag": "a",
-                                    "text": "点击此处",
+                                    "text": "click here",
                                     "href": "https://install.appcenter.ms/orgs/aelf-web/apps/DID-IOS"
                                 },
                                 {
                                     "tag": "text",
-                                    "text": "进行查看和下载"
+                                    "text": "check and download"
                                 },
                                 {   
                                     "tag": "img",
@@ -89,12 +83,9 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
      # Example: Upload main branch app binary to HockeyApp using the API
     if [ "$PLATFORM_TYPE" == "ios-testFlight" ];
      then
-        # curl -X POST -H "Content-Type: application/json" \
-        # -d '{"msg_type":"text","content":{"text":"DID:IOS端最新包（id:'$APPCENTER_BUILD_ID'） 已build成功并推送到testFlight，您可在testFlight查看最新版本的DID应用,并进行内部测试"}}' \
-        # https://open.feishu.cn/open-apis/bot/v2/hook/f2d3fffd-c630-4e59-86e3-e7053a64e4b2
 
         curl -X POST -H "Content-Type: application/json" \
-         -d '{"msg_type":"text","content":{"text":"DID:IOS端最新包（id:'$APPCENTER_BUILD_ID'） 已build成功并推送至TestFlight，您可申请进入内部测试组，并在TestFlight查看下载最新版本的DID应用"}}' \
+         -d '{"msg_type":"text","content":{"text":"The latest ios package(id:'$APPCENTER_BUILD_ID') has been completed and successfully pushed to testFlight, you can apply to join the internal testing group and view/download the latest version of the app on testFlight."}}' \
         https://open.feishu.cn/open-apis/bot/v2/hook/f2d3fffd-c630-4e59-86e3-e7053a64e4b2
 
     else
