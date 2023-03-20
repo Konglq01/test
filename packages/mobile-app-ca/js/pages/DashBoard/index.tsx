94ae2e0aaeb1b484b4608b08e2931521ac4470e0
Portkey-David <alex@portkey.finance>
2023-03-20 19:51:39 +0800

    refactor: 💡 useless code

--- packages/mobile-app-ca/js/pages/DashBoard/index.tsx
+++ packages/mobile-app-ca/js/pages/DashBoard/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Card from './Card';
-import { StyleSheet } from 'react-native';
 import { useAppCommonDispatch } from '@portkey-wallet/hooks/index';
 import DashBoardTab from './DashBoardTab';
-import { defaultColors } from 'assets/theme';
 import useEffectOnce from 'hooks/useEffectOnce';
 import { fetchTokenListAsync } from '@portkey-wallet/store/store-ca/assets/slice';
 import { useGetCurrentCAViewContract } from 'hooks/contract';
